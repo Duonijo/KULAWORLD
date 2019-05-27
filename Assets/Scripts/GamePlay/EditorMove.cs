657b6f3900d000e88038122bde7cf75f413e43ec
Haxaire <280125@supinfo.com>
2019-05-27 17:38:18 +0200

    Editor Level early dev dev MoveForward part two

---
+++ Assets/Scripts/GamePlay/EditorMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EditorMove : MonoBehaviour
+{
+    private bool editorMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        editorMode = SceneManager.GetActiveScene().name == "Editor";
+        print(editorMode);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!editorMode) return;
+        if (Input.GetKey("right"))
+        {
+            var transform1 = transform;
+            transform.position = Vector3.MoveTowards(transform1.position, transform1.forward,0);
+        }
+        else if (Input.GetKeyDown("up"))
+        {
+            print(transform.position);
+            transform.position+=Vector3.forward;
+            print(transform.position);
+            print(transform.position+Vector3.forward);
+
+
+        }
+    }
+    
+    
+}
---
+++ Assets/Scripts/UI/Editor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using EventTrigger = UnityEngine.Analytics.EventTrigger;
+
+namespace UI
+{
+    public class Editor : MonoBehaviour
+    {
+        private GameObject prefab;
+        public void InstantiatePrefabs(Button button)
+        {
+            var path = "Map_Asset/PREFAB/Models/" + button.name;
+            var loadPrefab = Resources.Load(path);
+            prefab = loadPrefab as GameObject;
+            Instantiate(prefab);
+            prefab.transform.position = new Vector3(0, 0, 0);
+            Debug.Log(SceneManager.GetActiveScene().name);
+            Debug.Log(SceneManager.GetActiveScene().buildIndex);
+
+        }
+        
+        public void OnClick(BaseEventData data)
+        {
+            PointerEventData pData = (PointerEventData)data;
+        }
+
+    }
+}
