3f92d00699e8da3a69deb85a62a3179c84ce1bb4
Haxaire <280125@supinfo.com>
2019-05-28 14:55:00 +0200

    Editor Interface Movement into Editor

---
+++ Assets/Scripts/Hierarchy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Hierarchy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+    
+}
